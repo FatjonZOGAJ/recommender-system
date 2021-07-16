42bdc135431b84a730b020d2f1b7e489b02bbf4a
FatjonZogaj <fatjon.zogaj@hotmail.com>
2021-07-16 17:05:53 +0200

    Add kernelNet outline

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -1,7 +1,8 @@
-from lib.models import svd, als, fm, fm_rel
+from lib.models import svd, als, kernelNet#, fm, fm_rel
 
 models = {'svd': svd,
           'als': als,
-          'fm': fm,
-          'fm_rel': fm_rel
+          'kernelNet': kernelNet,
+          # 'fm': fm,
+          # 'fm_rel': fm_rel
           }
