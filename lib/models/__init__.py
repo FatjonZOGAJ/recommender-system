89e003a2e6aa317fe2b78db021fe5030f698f177
Patrik <okanovic.patrik@gmail.com>
2021-07-24 21:30:53 +0200

    fix autorec

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -1,8 +1,9 @@
-from lib.models import autoencoder, fm, kernel_net, nmf, svd
+from lib.models import autoencoder, fm, kernel_net, nmf, svd, autorec
 
 models = {'svd': svd,
           'als': nmf,
           'kernel_net': kernel_net,
           'autoencoder': autoencoder,
+          'autorec': autorec,
           'fm': fm
           }
